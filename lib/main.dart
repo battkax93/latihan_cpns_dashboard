24c3e8af93ebb434bf623fc759ce64d1a7341734
choputra <choputra@github.com>
2021-01-02 22:54:36 +0700

    add dialog selector

--- lib/main.dart
+++ lib/main.dart
@@ -35,37 +35,6 @@ class dashBoard extends StatefulWidget {
 }
 
 class _dashBoardState extends State<dashBoard> {
-
-  void showDialog() {
-    showGeneralDialog(
-      barrierLabel: "Barrier",
-      barrierDismissible: true,
-      barrierColor: Colors.black.withOpacity(0.5),
-      transitionDuration: Duration(milliseconds: 700),
-      context: context,
-      pageBuilder: (_, __, ___) {
-        return Align(
-          alignment: Alignment.bottomCenter,
-          child: Container(
-            height: 300,
-            child: SizedBox.expand(child: FlutterLogo()),
-            margin: EdgeInsets.only(bottom: 50, left: 12, right: 12),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(40),
-            ),
-          ),
-        );
-      },
-      transitionBuilder: (_, anim, __, child) {
-        return SlideTransition(
-          position: Tween(begin: Offset(0, 1), end: Offset(0, 0)).animate(anim),
-          child: child,
-        );
-      },
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -82,7 +51,6 @@ class _dashBoardState extends State<dashBoard> {
               children: [
                 InkWell(
                   onTap: () {
-                    showDialog();
                   },
                   child: Container(
                     margin: EdgeInsets.all(10),
@@ -90,7 +58,7 @@ class _dashBoardState extends State<dashBoard> {
                     width: 250,
                     height: 125,
                     decoration: BoxDecoration(
-                      color: Colors.blue[400],
+                        color: Colors.blue[400],
                         boxShadow: [
                           BoxShadow(
                             color: Colors.black,
@@ -128,10 +96,7 @@ class _dashBoardState extends State<dashBoard> {
               children: [
                 InkWell(
                   onTap: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => ViewSoal(jenisSoal: 'TKP',)));
+                    showDialog();
                   },
                   child: Container(
                     margin: EdgeInsets.all(10),
@@ -224,4 +189,109 @@ class _dashBoardState extends State<dashBoard> {
       ),
     );
   }
+
+  void changePage(String jenisSoal){
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) => ViewSoal(
+              jenisSoal: jenisSoal,
+            )));
+  }
+
+  void showDialog() {
+    showGeneralDialog(
+      barrierLabel: "Barrier",
+      barrierDismissible: true,
+      barrierColor: Colors.black.withOpacity(0.5),
+      transitionDuration: Duration(milliseconds: 700),
+      context: context,
+      pageBuilder: (_, __, ___) {
+        return Center(
+          child: Container(
+            height: 300,
+            width: 300,
+            padding: EdgeInsets.all(20),
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10)),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "PILIH JENIS SOAL",
+                  style:
+                      TextStyle(color: const Color(0xFF1BC0C5), fontSize: 25),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                RaisedButton(
+                  onPressed: () {
+                    changePage('TIU');
+                  },
+                  child: Container(
+                    width: 70,
+                    height: 50,
+                    child: Center(
+                      child: Text(
+                        "TIU",
+                        style: TextStyle(color: Colors.white, fontSize: 30),
+                      ),
+                    ),
+                  ),
+                  color: const Color(0xFF1BC0C5),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                RaisedButton(
+                  onPressed: () {
+                   changePage('TWK');
+                  },
+                  child: Container(
+                    width: 70,
+                    height: 50,
+                    child: Center(
+                      child: Text(
+                        "TWK",
+                        style: TextStyle(color: Colors.white, fontSize: 30),
+                      ),
+                    ),
+                  ),
+                  color: const Color(0xFF1BC0C5),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                RaisedButton(
+                  onPressed: () {
+                   changePage('TKP');
+                  },
+                  child: Container(
+                    width: 70,
+                    height: 50,
+                    child: Center(
+                      child: Text(
+                        "TKP",
+                        style: TextStyle(color: Colors.white, fontSize: 30),
+                      ),
+                    ),
+                  ),
+                  color: const Color(0xFF1BC0C5),
+                )
+              ],
+            ),
+          ),
+        );
+      },
+      transitionBuilder: (_, anim, __, child) {
+        return SlideTransition(
+          position: Tween(begin: Offset(0, 1), end: Offset(0, 0)).animate(anim),
+          child: child,
+        );
+      },
+    );
+  }
+
 }
